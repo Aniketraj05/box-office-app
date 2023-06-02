5b826da2ce558aa9ef76a73ce86a1f3314411356
Aniket Raj <aniketraj6039@gmail.com>
2023-06-02 18:03:43 +0530

    Installed react-router-dom, Added some pages, updated package.json

--- src/App.js
+++ src/App.js
@@ -1,5 +1,20 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Contact from "./pages/Contact";
+import NoMatch from "./pages/NoMatch";
+
 function App() {
-  return <div className="app">Hello</div>;
+  return (
+    <div className="app">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="*" element={<NoMatch />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
